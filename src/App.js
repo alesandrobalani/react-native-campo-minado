d66b83c14c019e234b1a302652a0e33dbda5ce84
adqt0019 <alesandro.balani@adiq.com.br>
2020-05-03 13:09:45 -0300

    campo minado

---
+++ src/App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+import params from './params'
+import Field from './components/Field'
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Iniciando Mines...</Text>
+      <Text style={styles.instructions}>Tamanho da grade:
+        {params.getColumnsAmount()}x{params.getRowsAmount()}
+      </Text>
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={4} />
+      <Field opened nearMines={5} />
+      <Field opened nearMines={6} />
+      <Field opened nearMines={7} />
+      <Field opened nearMines={8} />
+      <Field mined />
+      <Field opened mined />
+      <Field opened mined exploded />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default App;
